945141238f20461dc7bed526f307c9f423f469b4
mbetts7 <mbetts7@gmail.com>
2014-06-09 21:18:13 -0700

    add Ian as recipient Mike as bcc

--- email.rb
+++ email.rb
@@ -3,20 +3,25 @@ require 'mandrill'
 class MyEmailer
   def send
     
-    m = Mandrill::API.new 
-    message = {  
-     :subject=> 'hello world',  
-     :from_name=> 'Mike Betts',  
-     :text=>'Hi Ian! Sending you an email via this API I just made.',  
-     :to=>[  
-       {  
-         :email=> 'mbetts7@gmail.com',
-         :name=> 'Ian'  
-       }  
-     ],  
-     :from_email=>'noreply@example.com'  
-    }  
-    sending = m.messages.send message  
+    m = Mandrill::API.new
+    message = {
+      :subject => 'hello world',
+      :from_name => 'Mike Betts',
+      :text => 'Hi Ian! Sending you an email via this API I just made.',
+      :to =>[
+        {
+          :email => 'iolsen@newrelic.com',
+          :name => 'Ian Olsen'
+        },
+        {
+          :email => 'mbetts7@gmail.com',
+          :name => 'Mike Betts',
+          :type => 'bcc'
+        } 
+      ],
+      :from_email =>'noreply@example.com'
+    }
+    sending = m.messages.send message
     puts sending
     
   end
